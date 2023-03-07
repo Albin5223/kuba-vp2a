53f54620bda3b558288ebd1c8261e2daa89f09dd
Sarowsss <bonjour2bonsoir@gmail.com>
2023-03-07 17:25:03 +0100

    Ajout fonctions de creation de noeud et calcul de point

--- src/main/java/model/NoeudIA.java
+++ src/main/java/model/NoeudIA.java
@@ -1,5 +1,6 @@
 package Model;
 
+import java.util.LinkedList;
 
 public class NoeudIA  {
 	private Plateau plateau;
@@ -7,14 +8,44 @@ public class NoeudIA  {
 	Joueur joueurAcc;
 	Joueur joueurAdv;
 	int diffPoint;
+	LinkedList<NoeudIA> fils;
 
-	public void predictForDepth () {
-		for (int i = 0; i<plateau.getLongueur(); i++) {
-			for (int j = 0; j<plateau.getLongueur(); j++) {
-				if (plateau.getBoard()[i][j]==joueurAcc.getColor()) {
-					
+	public NoeudIA (NoeudIA parent) throws CloneNotSupportedException {
+		plateau = parent.plateau.clone();
+		this.parent = parent;
+		joueurAcc = parent.joueurAdv;
+		joueurAdv = parent.joueurAcc;
+	}
+
+	public void createNextNode () throws CloneNotSupportedException {
+		for (int i = 0; i<joueurAdv.tabBilles.length ; i++) {
+			if (joueurAdv.tabBilles[i].i!=-1) {
+				if (plateau.push(joueurAdv.tabBilles[i],Direction.NORTH,joueurAdv,joueurAcc)==State.SUCCESS) {
+					fils.add(new NoeudIA(this));
+					plateau.undoLastMove();
+				}
+				if (plateau.push(joueurAdv.tabBilles[i],Direction.WEST,joueurAdv,joueurAcc)==State.SUCCESS) {
+					fils.add(new NoeudIA(this));
+					plateau.undoLastMove();
+				}
+				if (plateau.push(joueurAdv.tabBilles[i],Direction.EAST,joueurAdv,joueurAcc)==State.SUCCESS) {
+					fils.add(new NoeudIA(this));
+					plateau.undoLastMove();
+				}
+				if (plateau.push(joueurAdv.tabBilles[i],Direction.SOUTH,joueurAdv,joueurAcc)==State.SUCCESS) {
+					fils.add(new NoeudIA(this));
+					plateau.undoLastMove();
 				}
 			}
+			
+		}
+	}
+
+
+	public void updateDiffPoint () {
+		for (NoeudIA n : this.fils) {
+			n.diffPoint = n.rateDiffMarble();
+			n.updateDiffPoint();
 		}
 	}
 
