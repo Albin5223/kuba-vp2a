8163ed63493d4ee0efe0df72e17f1cd6ddc38f99
Meril LEFORESTIER <meril.leforestier.3@gmail.com>
2023-03-13 18:34:24 +0100

    correction erreur

--- src/main/java/Model/Defi.java
+++ src/main/java/Model/Defi.java
@@ -1,3 +1,5 @@
+package Model;
+
 import Model.*;
 import java.util.LinkedList;
 
@@ -5,21 +7,16 @@ public class Defi extends Model {
 
 	private int numero ; 	
 	private Direction [][] coups;
-	private (int,int) [][] poscoup= ;
+	private int [][] casex;
+	private int [][] casey ;
 
 	public Defi(int n){
-
-        observeurs= new LinkedList<>();
-        joueurs = new Joueur[2];
-        plat = new Plateau(2);
+		super(2);
 
         initialiseDefi(0);
-        Joueur j1 = new Joueur(Colour.WHITE,n);
-        Joueur j2 = new Joueur(Colour.BLACK,n);
-        state=State.SUCCESS;
-        joueurs[0] = j1;
-        joueurs[1] = j2;
+
         numero = n;
+
     }
 
     public void initialiseDefi(int x){
