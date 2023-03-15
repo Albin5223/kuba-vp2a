ea03cdb2230e6254143a0076b00f1ec5bea7f685
Sarowsss <bonjour2bonsoir@gmail.com>
2023-03-15 22:46:37 +0100

    Ajout de minimax et de fonction permettant le fonctionnement de l'IA

--- src/main/java/model/NoeudIA.java
+++ src/main/java/model/NoeudIA.java
@@ -4,36 +4,54 @@ import java.util.LinkedList;
 
 public class NoeudIA  {
 	private Plateau plateau;
-	private NoeudIA parent;
 	Joueur joueurAcc;
 	Joueur joueurAdv;
-	int diffPoint;
+	int value;
+	Direction dir;
+	Pos pos;
 	LinkedList<NoeudIA> fils;
 
-	public NoeudIA (NoeudIA parent) throws CloneNotSupportedException {
-		plateau = parent.plateau.clone();
-		this.parent = parent;
-		joueurAcc = parent.joueurAdv;
-		joueurAdv = parent.joueurAcc;
+	public NoeudIA (Plateau p, Joueur joueurAcc, Joueur joueurAdv) throws CloneNotSupportedException {
+		plateau = p.clone();
+		this.joueurAcc=joueurAcc;
+		this.joueurAdv=joueurAdv;
 	}
 
-	public void createNextNode () throws CloneNotSupportedException {
+	public NoeudIA (NoeudIA n) throws CloneNotSupportedException {
+		plateau = plateau.clone();
+		joueurAcc = n.joueurAdv;
+		joueurAdv = n.joueurAcc;
+	}
+
+	public void createNextNodes () throws CloneNotSupportedException {
 		for (int i = 0; i<joueurAdv.tabBilles.length ; i++) {
 			if (joueurAdv.tabBilles[i].i!=-1) {
 				if (plateau.push(joueurAdv.tabBilles[i],Direction.NORTH,joueurAdv,joueurAcc)==State.SUCCESS) {
-					fils.add(new NoeudIA(this));
+					NoeudIA newNode = new NoeudIA(this);
+					newNode.value=newNode.rateValue(this);
+					newNode.dir=Direction.NORTH;
+					fils.add(newNode);
 					plateau.undoLastMove();
 				}
 				if (plateau.push(joueurAdv.tabBilles[i],Direction.WEST,joueurAdv,joueurAcc)==State.SUCCESS) {
-					fils.add(new NoeudIA(this));
+					NoeudIA newNode = new NoeudIA(this);
+					newNode.value=newNode.rateValue(this);
+					newNode.dir=Direction.WEST;
+					fils.add(newNode);
 					plateau.undoLastMove();
 				}
 				if (plateau.push(joueurAdv.tabBilles[i],Direction.EAST,joueurAdv,joueurAcc)==State.SUCCESS) {
-					fils.add(new NoeudIA(this));
+					NoeudIA newNode = new NoeudIA(this);
+					newNode.value=newNode.rateValue(this);
+					newNode.dir=Direction.EAST;
+					fils.add(newNode);
 					plateau.undoLastMove();
 				}
 				if (plateau.push(joueurAdv.tabBilles[i],Direction.SOUTH,joueurAdv,joueurAcc)==State.SUCCESS) {
-					fils.add(new NoeudIA(this));
+					NoeudIA newNode = new NoeudIA(this);
+					newNode.value=newNode.rateValue(this);
+					newNode.dir=Direction.SOUTH;
+					fils.add(newNode);
 					plateau.undoLastMove();
 				}
 			}
@@ -41,23 +59,57 @@ public class NoeudIA  {
 		}
 	}
 
+	public static Move determineBestMove (Plateau p, int depth, Joueur joueurAcc, Joueur joueurAdv) throws CloneNotSupportedException {
+		NoeudIA arbre = createTree(p, depth, joueurAcc, joueurAdv);
+		int bestValue = -999999999;
+		NoeudIA bestNode = arbre.fils.getFirst(); // Faire un cas de fin de partie si erreur
+		for (NoeudIA node : arbre.fils) {
+			int mm = node.minimax(depth);
+			if (bestValue < mm ) {
+				bestValue = mm;
+				bestNode = node;
+			}
+		}
+		Move bestMove = new Move(bestNode.pos,bestNode.dir);
+		return bestMove;
+	}
 
-	public void updateDiffPoint () {
-		for (NoeudIA n : this.fils) {
-			n.diffPoint = n.rateDiffMarble();
-			n.updateDiffPoint();
+	public static NoeudIA createTree (Plateau p, int depth, Joueur joueurAcc, Joueur joueurAdv) throws CloneNotSupportedException {
+		NoeudIA tete = new NoeudIA(p,joueurAcc,joueurAdv);
+		createTreeRec(tete, depth);
+		return tete;
+	}
+
+	public static void createTreeRec (NoeudIA n, int depth) throws CloneNotSupportedException {
+		if (depth == 0) { return; }
+		n.createNextNodes();
+		for (NoeudIA node : n.fils) {
+			createTreeRec(node, depth - 1);
 		}
 	}
 
-
-
-
-	public int rateDiffMarble () {
-		if (this.parent==null) {
-			return 0;
+	public int minimax (int depth) {
+		if (depth == 0 || this.fils.isEmpty()) {
+			return this.value;
 		}
-		return ((-this.joueurAcc.getBilles()+parent.joueurAdv.getBilles())*2+
-		this.plateau.billesRouges-parent.plateau.billesRouges);
+		if (this.joueurAcc.getColor()==Colour.WHITE) {
+			int m = -999999999;
+			for (NoeudIA node : fils) {
+				m = Math.max(m, node.minimax(depth - 1));
+			}
+			return m;
+		}
+		else {
+			int m = 999999999;
+			for (NoeudIA node : fils) {
+				m = Math.min(m, node.minimax(depth - 1));;
+			}
+			return m;
+		}
+	}
+
+	public int rateValue (NoeudIA parent) { //A FAIRE
+		return 0;
 	}
 
 }
