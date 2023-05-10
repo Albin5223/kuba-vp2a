b242376fe50d1ff4275435679b0bb3d7a8e34ef1
stormneed <matheodesousa935@gmail.com>
2023-05-11 01:18:09 +0200

    RACCOURCIIII sur edition

--- src/main/java/GUI/ViewEdition.java
+++ src/main/java/GUI/ViewEdition.java
@@ -1,6 +1,10 @@
 package GUI;
 
-import javax.swing.JFrame;
+import javax.swing.*;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.StyledDocument;
+import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import Controleur.*;
@@ -8,19 +12,51 @@ import SearchFile.*;
 
 public class ViewEdition extends View{
 
+	JTextPane racc;
+
     PopUp popUp;
     public ViewEdition(int nb, JFrame l) {
         super(nb, l);
-        
+
     }
 
-    public void initialisation(){
-        popUp = new PopUp("Mode solution activee");
-		popUp.setBounds(plateau.getWidth()/2-100,getWidth()/2-50,250,100);
-        popUp.setVisible(false);
-        conteneur.add(popUp);
+    public void initialisation() {
+
+		JPanel bg = new JPanel(new BorderLayout()) {
+			protected void paintComponent(Graphics g) {
+				super.paintComponent(g);
+				g.drawImage(BanqueImage.images[9], -15, 0, null);
+			}
+		};
+		bg.setOpaque(false);
+
+		racc = new JTextPane();
+
+		StyledDocument doc = racc.getStyledDocument();
+		SimpleAttributeSet center = new SimpleAttributeSet();;
+		racc.setForeground(new Color(238, 179, 97));
+		racc.setEditable(false);
+		racc.setOpaque(false);
+		StyleConstants.setAlignment(center, StyleConstants.ALIGN_CENTER);
+		doc.setParagraphAttributes(0, doc.getLength(), center, false);
+		racc.setDocument(doc);
+		racc.setText("Mode résolution: Ctrl+R \n Sauvegarder niveau: Ctrl+S");
+		racc.setFont(new Font("Impact", Font.PLAIN, 15));
+
+		racc.setBounds(70,60,150,150);
+
+
+		bg.setBounds(plateau.getX() + plateau.getWidth(), jv2.getY(), 300, 180);
+		popUp = new PopUp("Mode solution activee");
+		popUp.setBounds(plateau.getWidth() / 2 - 100, getWidth() / 2 - 50, 250, 100);
+		popUp.setVisible(false);
+		bg.setVisible(true);
+		conteneur.add(popUp);
+		bg.add(racc);
+		bg.add(new JLabel(""));
+		conteneur.add(bg);
 		conteneur.repaint();
-    }
+	}
 
 
     public void addCtrlEditeur (ControleurEditeur ctrl){
